c12ab7f53f2d92410efd719bdc62a2a2ff77e9bc
Abhishek <shekabhi1208@gmail.com>
2021-07-29 00:27:53 +0530

    Update bubble_sort.c

--- coding/bubble_sort.c
+++ coding/bubble_sort.c
@@ -64,78 +64,18 @@ void bubble_sort(int *arr, int n) {
 }
 
 
-//function to perform selection sort algo on passed array
-void selection_sort(int *arr, int n) {
-	
-	
-	for (int i = 0; i < n-1; i++){
-    	
-		int min_idx = i;
-		//finding the minimum element in the range A[i...n] 
-        for (int j = i+1; j < n; j++) {
-	        if (arr[j] < arr[min_idx]) {
-	            min_idx = j;
-			}
-		}
-     
-		//placing current minimum to its correct position by swapping     
-	    swap(&arr[min_idx], &arr[i]);
-	}
-		
-}
-
-
-//function to perform insertion sort algo on passed array
-void insertion_sort(int *arr, int n) {
-	
-	for(int i = 1; i < n; i++) {
-		int curr_element = arr[i];
-		int j = i;
-		
-		//finding correct position for the current element in the already sorted array [0... i-1]
-		
-		while(j > 0 && arr[j - 1] > curr_element) {
-			arr[j] = arr[j - 1];
-			j--; 
-		} 
-		
-		arr[j] = curr_element;
-	
-	}
-	
-}
 
 
 //time calculator to analyze performance of bubble Sort on various type of inputs
-long long time_calculator(int *arr, int n, int algo_no) {
+long long time_calculator(int *arr, int n) {
 	struct timeval start;	
 	struct timeval end;	
 	long long time_taken;	
-
-
-	//performing the algo specified by the user and marking the time taken by it
-	switch (algo_no) {
-		case 1: {
-			gettimeofday(&start, 0);	// mark the start time 
-			insertion_sort(arr, n);			
-			gettimeofday(&end, 0);		// mark the end time 
-			
-			break;
-		}
-		case 2: {
-			gettimeofday(&start, 0);	// mark the start time 
-			selection_sort(arr, n);			
-			gettimeofday(&end, 0);		// mark the end time 
-			break;
-		}
-		case 3: {
-			gettimeofday(&start, 0);	// mark the start time 
-			bubble_sort(arr, n);			
-			gettimeofday(&end, 0);		// mark the end time 
-			break;
-		}
-	}
-
+	
+	gettimeofday(&start, 0);	// mark the start time 
+	bubble_sort(arr, n);			
+	gettimeofday(&end, 0);		// mark the end time 
+	
 	//formula to convert seconds to microseconds
 	time_taken = ((end.tv_sec * 1000000) + end.tv_usec) - ((start.tv_sec * 1000000) + start.tv_usec);
 	return time_taken;
@@ -145,12 +85,6 @@ long long time_calculator(int *arr, int n, int algo_no) {
 //driver function
 int main() {
 	
-	printf("1. Insertion Sort\n");
-	printf("2. Selection Sort\n");
-	printf("3. Bubble Sort\n");
-	printf("Enter the Algorithm Number you want to perform: ");
-	int algo_no;
-	scanf("%d", &algo_no);
 	
 	int test_cases;
 	printf("Enter The number of TestCases: ");
@@ -163,16 +97,16 @@ int main() {
 		
 		long long time_taken;
 		random_input_generator(arr, n);
-		time_taken = time_calculator(arr, n, algo_no);
-		printf("Time taken by specified algorithm for random input of size %d is %d \n", n, time_taken);
+		time_taken = time_calculator(arr, n);
+		printf("Time taken by Bubble Sort algorithm for random input of size %d is %d \n", n, time_taken);
 		
 		increasing_input_generator(arr, n);
-		time_taken = time_calculator(arr, n, algo_no);
-		printf("Time taken by specified algorithm for increasing input of size %d is %d \n", n, time_taken);
+		time_taken = time_calculator(arr, n);
+		printf("Time taken by Bubble Sort algorithm for increasing input of size %d is %d \n", n, time_taken);
 		
 		decreasing_input_generator(arr, n);
-		time_taken = time_calculator(arr, n, algo_no);
-		printf("Time taken by specified algorithm for decreasing input of size %d is %d \n\n", n, time_taken);
+		time_taken = time_calculator(arr, n);
+		printf("Time taken by Bubble Sort algorithm for decreasing input of size %d is %d \n\n", n, time_taken);
 		
 	}
 	
