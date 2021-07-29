349db0674cf6c53f72ed16055d475951dbf7e442
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:30:20 +0530

    program to print pyramid of length n

--- coding/pyramid.cpp
+++ coding/pyramid.cpp
@@ -1,3 +1,4 @@
+//program to print pyramid of given length
 #include <stdio.h>
 int main() {
 	printf("Enter height of pyramid: ");
@@ -5,15 +6,20 @@ int main() {
 	scanf("%d", &height);
 	
 	for(int i = 1; i <= height; i++) {
+		//printing the ith line of pyramind after printing required number of spaces
+		
 		for(int space = 1; space <= height - i; space++) {
 			printf(" ");
 		}
+		
 		for(int count = 1; count <= i; count++) {
 			printf("# ");
 		}
+		
 		for(int space = 1; space < height - i; space++) {
 			printf(" ");
 		}
+		
 		printf("\n");
 	}
 	
