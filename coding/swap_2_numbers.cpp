0852cfe64e05bb4a34a8bb33eca026d46a505478
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:55:46 +0530

    program to swap given 2 numbers without using 3rd variable

--- coding/swap_2_numbers.cpp
+++ coding/swap_2_numbers.cpp
@@ -1,3 +1,4 @@
+//program to swap given 2 numbers without using 3rd variable
 #include <stdio.h>
 
 int main() {
@@ -7,7 +8,7 @@ int main() {
 	scanf("%d %d", &a, &b);
 	printf("number before swapping:\n a = %d \n b = %d\n", a, b);
 	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;
+	b = a ^ b;   // it becomes b = (a ^ b) ^b = a
+	a = a ^ b;   // it becomes a = (a ^ b) ^a = b
 	printf("number after swapping:\n a = %d \n b = %d", a, b);
 }
