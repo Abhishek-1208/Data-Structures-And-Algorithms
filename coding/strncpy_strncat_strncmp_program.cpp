1bc16a13d4f88cfac8675ae8a25d6daec2d2241e
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:50:03 +0530

    implementing strncat, strncpy, strncmp

--- coding/strncpy_strncat_strncmp_program.cpp
+++ coding/strncpy_strncat_strncmp_program.cpp
@@ -1,3 +1,4 @@
+//program to implement the standard string.h library functions such as strncpy , strncat, strncmp
 #include<stdio.h>
 //return length of entered string as return value
 int input_string(char *str)  {
@@ -13,10 +14,11 @@ int input_string(char *str)  {
 	}
 	return len;
 }
-
+//function to return the minimum of passed 2 variable
 int min(int a, int b) {
 	return a <= b ? a: b;
 }
+//function to calculate length of passed array
 int strlen(char *str) {
 	int len = 0;
 	while(str[len] != '\0') {
@@ -24,13 +26,15 @@ int strlen(char *str) {
 	}
 	return len;
 }
-
+//function to implement the strncpy function which copies the first n characters of str2 to str1
+//returns the length of modified string
 int strncpy(char *str1, char *str2, int n) {
 	
 	int len1 = strlen(str1);
 	int len2 = strlen(str2);
+	//finding the minimum number of character that str1 can hold..this condition is necessary to avoid overflow incase len1 and n are way larger than len1
 	n = min(n, min(len1, len2));
-	
+	//copying the character of str2 to str1 one by one
 	for(int i = 0; i < n; i++) {
 		str1[i] = str2[i];
 	}
@@ -39,14 +43,15 @@ int strncpy(char *str1, char *str2, int n) {
 	return n;
 	
 }
-
+//function to implement strncat which concatenate the first n characters from str2 to the end of str1
+//returns the length of modified string
 int strncat(char *str1, char *str2, int n) {
 	
 	
 	int len1 = strlen(str1);
 	int len2 = strlen(str2);
 	n = min(n, len2);
-	
+	//concatenating the first n characters of str2 at the end of str1
 	for(int i = 0; i < n; i++) {
 		str1[len1++] = str2[i];
 	}
@@ -56,6 +61,8 @@ int strncat(char *str1, char *str2, int n) {
 		
 }
 
+//function to implement the strncmp function which compares 2 strings upto first n characters 
+//returns 0 if str1 == str2 or some (x >0) if str1 > str2 or some (x<0) if str1 < str2 
 int strncmp(char *str1, char *str2, int n) {
 	
 	
@@ -63,6 +70,7 @@ int strncmp(char *str1, char *str2, int n) {
 	int len2 = strlen(str2);
 	n = min(n, min(len1, len2));
 	for(int i = 0; i < n; i++) {
+		//incase characters are not matching ..return the difference between their ascii value
 		if(str1[i] != str2[i]) {
 			return str1[i] - str2[i];
 		}	
@@ -86,6 +94,7 @@ int main() {
 	printf("Enter the 2nd string: ");
 	int len2 = input_string(str2);
 	
+	//checking whether the passed 2 strings are equal or not upto 5 characters
 	int status = strncmp(str1, str2, 5);
 	if(status == 0 ){
 		printf("strings are equal\n");
@@ -97,9 +106,11 @@ int main() {
 		printf("stirng1 is greater than string2\n");
 	}
 	
+	//to check the correct working of strncat function
 	len1 = strncat(str1, str2, 5);
 	printf("string1 after cancatenation becomes %s\n", str1);
 	
+	//to check the correct working of strncpy function
 	len1 = strncpy(str1, str2, 5);
 	printf("string1 after copying becomes %s\n", str1);
 	
