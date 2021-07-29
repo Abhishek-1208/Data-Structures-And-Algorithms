daba4de4461b2d4836e2874a5ceb438fa385f269
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:06:29 +0530

    find first index where any character of string2 is found

--- coding/any.cpp
+++ coding/any.cpp
@@ -1,6 +1,7 @@
+//program to find first occurence of any character of string2 in string1
 #include <stdio.h>
 
-//return length of entered string as return value
+//return length of entered string after taking input from user
 int input_string(char *str)  {
 	char temp;
 	int len = 0;
@@ -14,6 +15,8 @@ int input_string(char *str)  {
 	}
 	return len;
 }
+
+//auxillary function to find if there exist the passed character in string2
 int is_present_in_2nd_string(char *str, int len, char x) {
 	for(int i = 0; i < len; i++) {
 		if(str[i] == x) {
@@ -22,26 +25,33 @@ int is_present_in_2nd_string(char *str, int len, char x) {
 	}
 	return 0;
 }
+
+//program to find the first index where occurence of any character of string2 is found.
 int any(char *str1, int len1, char *str2, int len2) {
 	
 	
 	for(int i = 0; i < len1; i++) {
+		//check if current character is present in string2 if so then return the index
 		if(is_present_in_2nd_string(str2, len2, str1[i]) ) {
 			return i;
 		}
 	}
-	
+	//reaching here means there is no character of string2 which is present in string1..hence returning -1
 	return -1;
 	
 }
 int main() {
 	char str1[50], str2[50];
+	
 	printf("Enter the 1st string: ");
 	int len1 = input_string(str1);
+	
 	printf("Enter the 2nd string: ");
 	int len2 = input_string(str2);
 	
+	//calling function to find the index
 	int index = any(str1, len1, str2, len2);
+	
 	if(index == -1) {
 		printf("There is no occurence of any character of string2 in string\n");
 	}
