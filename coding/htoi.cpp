f86fb16ecf64a6afc64d2f4c63d7f181ce3c6686
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:18:24 +0530

    program to convert hexadecimal to decimal number

--- coding/htoi.cpp
+++ coding/htoi.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-//gives value cooresponding to given character
+//return length of entered string as return value after taking input from user
+int input_string(char *str)  {
+	char temp;
+	int len = 0;
+	while(temp = getchar()) {
+		if(temp =='\n') {
+			str[len] ='\0';
+			break;
+		}
+		else
+			str[len++] = temp; 
+	}
+	return len;
+}
+
+//give the value in decimal representation cooresponding to given hexadeciaml character
 int corr_val(char x) {
 	
 	if(x == 'a' || x == 'A') {
@@ -22,10 +37,11 @@ int corr_val(char x) {
 		return 15;
 	}
 	
-	//reaching here means the x is numeric value
+	//reaching here means that x is numeric value
 	return x - '0';
 	
 }
+//function to calculate x raise to power y
 int pow(int x, int y) {
 	int num = 1;
 	for(int i = 1; i <= y; i++) {
@@ -34,16 +50,20 @@ int pow(int x, int y) {
 	return num;
 }
 
+//auxillary function to convert hexadecimal string to its decimal value
 int htoi(char *str, int len) {
 	int num = 0;
 	
 	for(int i = len - 1; i >= 0; i--) {
+		// str[i] == x or X means we have processes all the characters
 		if(str[i] == 'x' || str[i] == 'X') {
 			break;
 		}
+		//adding the contribution of current character to the final result
 		else {
 			num += pow(16, len - i - 1)*corr_val(str[i]);
 		}	
+		
 	}
 	
 	return num;
@@ -51,16 +71,9 @@ int htoi(char *str, int len) {
 int main() {
 	char str[50], temp;
 	printf("Enter the string: ");
-	int len = 0;
-	while(temp = getchar()) {
-		if(temp =='\n') {
-			str[len] ='\0';
-			break;
-		}
-		else
-			str[len++] = temp; 
-	}
+	int len = input_string(str);
 	
+	//calling function to get value corresponding to given hexadecimal number
 	int num = htoi(str, len);
 	printf("number corresponding to hexadecimal %s in decimal form is %d\n", str, num);
 	
