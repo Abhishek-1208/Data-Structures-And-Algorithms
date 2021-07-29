340dd97e90f1e5f128fa1dd4c65f1270ab1fe61f
Abhishek <shekabhi1208@gmail.com>
2021-07-29 22:37:56 +0530

    program to check if the pattern is present at the end of string or not

--- coding/strend.cpp
+++ coding/strend.cpp
@@ -1,3 +1,4 @@
+//program to check if the pattern is present at the end of string or not
 #include<stdio.h>
 //return length of entered string as return value
 int input_string(char *str)  {
@@ -14,18 +15,20 @@ int input_string(char *str)  {
 	return len;
 }
 
-
+//helper function to check if the patter is present or not at the end of given string
 int  strend(char *str, int len1, char *pat, int len2) {
     if(len2 > len1)
     	return 0;
     
 	for(int i = len1 -1 , j = len2 - 1; j >= 0; j--, i-- ) {
+		//if any character from end is not equal with character of given string...then the pattern does'nt exist at the end so return false
 		if(str[i] != pat[j]) {
 			return 0;
 		}
+		
 	}	
     
-    
+    //reaching at this point means all character of the given pattern were found at the end of string ..so returning true
     return 1;
 }
 
@@ -37,7 +40,7 @@ int main() {
 	
 	printf("Enter the patter to be searched: ");
 	int len2 = input_string(pat);
-
+	//function to check if pattern exist at the end of string or not
 	int index =  strend(str, len1, pat, len2);
 	printf("%d", index);
 	
